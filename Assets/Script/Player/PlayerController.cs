ca25e23bca2b5ff30d1c2070ba61e4b4b71a68dc
eli0504 <2003ecc@gmail.com>
2024-01-31 22:10:58 +0100

    flip box collider

--- Assets/Script/Player/PlayerController.cs
+++ Assets/Script/Player/PlayerController.cs
@@ -144,13 +144,14 @@ public class PlayerController : MonoBehaviour
         if (horizontalInput > 0f) //right direction
         {
             anim.SetBool("running", true);
-            sprite.flipX = false; //for flip to the right direction
-            
+            //sprite.flipX = false; //for flip to the right direction
+            player.transform.localScale = new Vector3(1, 1, 1);
         }
         else if (horizontalInput < 0f) //left direction
         {
             anim.SetBool("running", true);
-            sprite.flipX = true;
+            // sprite.flipX = true;
+            player.transform.localScale = new Vector3(-1, 1, 1);
         }
         else
         {
