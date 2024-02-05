1fcb3563f75ac12892233efe4755ea7ca31c3186
eli0504 <2003ecc@gmail.com>
2024-02-05 13:17:25 +0100

    enemy chase

--- Assets/Script/enemyController.cs
+++ Assets/Script/enemyController.cs
@@ -6,10 +6,16 @@ public class enemyController : MonoBehaviour
 {
     public GameObject pointA;
     public GameObject pointB;
+
     private Rigidbody2D rb;
     private Animator anim;
+
     private Transform currentPoint;
     public float speed;
+
+    public Transform player;//player pos
+    public bool isChasing;
+    public float ChaseDistance; //how close you can get before the enemy chases the player
 
     private void Start()
     {
@@ -21,9 +27,14 @@ public class enemyController : MonoBehaviour
 
     private void Update()
     {
+        Patrol();
+    }
+
+    private void Patrol()
+    {
         Vector2 point = currentPoint.position - transform.position; //direction my enemy wants to go
         //go to direction A to B
-        if(currentPoint == pointB.transform)
+        if (currentPoint == pointB.transform)
         {
             rb.velocity = new Vector2(speed, 0);
         }
@@ -33,16 +44,42 @@ public class enemyController : MonoBehaviour
         }
 
         //if enemy reach the current point
-        if(Vector2.Distance(transform.position, currentPoint.position) < 2f  && currentPoint == pointB.transform)
+        if (Vector2.Distance(transform.position, currentPoint.position) < 2f && currentPoint == pointB.transform)
         {
             Flip();
             currentPoint = pointA.transform;
         }
-        if(Vector2.Distance(transform.position, currentPoint.position) < 2f && currentPoint == pointA.transform)
+        if (Vector2.Distance(transform.position, currentPoint.position) < 2f && currentPoint == pointA.transform)
         {
             Flip();
             currentPoint = pointB.transform;
         }
+    }
+
+    private void Chase()
+    {
+        if(isChasing)
+        {
+            if(transform.position.x > player.position.x)
+            {
+                transform.position += Vector3.left * speed * Time.deltaTime;
+            }
+            if (transform.position.x < player.position.x)
+            {
+                transform.position += Vector3.right * speed * Time.deltaTime;
+            }
+        }
+        else
+        {
+            //check if the playe is closer
+            if(Vector2.Distance(transform.position, player.position) < ChaseDistance)
+            {
+                isChasing = true;
+            }
+
+            Patrol();
+        }
+
     }
 
     private void Flip()
