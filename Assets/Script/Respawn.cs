11b957695986dd10c6525a737b1deebc0af85147
eli0504 <2003ecc@gmail.com>
2024-02-06 21:22:39 +0100

    checkpoint/respawnpoint

---
+++ Assets/Script/Respawn.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject respawnoint;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            player.transform.position = respawnoint.transform.position;
+        }
+    }
+}
