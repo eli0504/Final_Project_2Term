f556704a1db258d3d4cb6571a6898e2c868765a7
eli0504 <2003ecc@gmail.com>
2024-02-17 15:02:23 +0100

    -

--- Assets/Script/Player/PlayerCombat.cs
+++ Assets/Script/Player/PlayerCombat.cs
@@ -19,12 +19,6 @@ public class PlayerCombat : MonoBehaviour
 
     }
 
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
